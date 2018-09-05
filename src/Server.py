2efee8e69c5fb94844349eb802b5546e9118dcc0
GrayCentipede <maucarrui@gmail.com>
2018-09-05 16:21:02 -0500

    Fixed send_msg_to method

--- src/Server.py
+++ src/Server.py
@@ -1,13 +1,16 @@
 from socket import AF_INET, socket, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from threading import Thread
 from Client import Client
+from Room import Room
 
 class Server(object):
     buffer_size = 1024
     host = None
     port = None
     server = None
+    lobbies = {}
     clients = {}
+    server_id = '.-.'
 
     class Conection(object):
         name = None
@@ -30,23 +33,38 @@ class Server(object):
         try:
             self.server.bind(self.address)
             self.server.listen(num_conections)
+            self.lobbies['global'] = Room('global')
             print('Server {} is now awaiting for connections at port {}'.format(host, port))
 
         except:
             print('An error has occurred while trying to lift the server')
 
     def send_msg_to(self, author, addressee, msg):
-        prefix = '@' + author + ' : '
+        is_server = False
+        client = None
+
+        if (str(author) == self.server_id):
+            prefix = '@' + self.server_id + ' : '
+            addressee.socket.send(bytes(prefix + msg).encode('utf8'))
+            return
+
+        prefix = '@' + author.name + ' : '
 
         for address in self.clients:
             client = self.clients[address]
             if (client.name == addressee):
                 break
 
+        if (not is_server) and (author.address != client.address):
+            author.socket.send(bytes(prefix + msg).encode('utf8'))
+
         client.socket.send(bytes(prefix + msg).encode('utf8'))
 
     def send_msg_to_all(self, author, msg):
-        prefix = '@' + author + ' :'
+        if (str(author) == self.server_id):
+            prefix = '@' + self.server_id + ' : '
+        else:
+            prefix = '@' + author.name + ' :'
         for address in self.clients:
             client = self.clients[address]
             client.socket.send(bytes(prefix + msg).encode('utf8'))
@@ -65,7 +83,7 @@ class Server(object):
                         If you ever want to quit, type: 'no one' when you're asked with who do you want to
                         talk to exit the chat.
                     """.format(client.name, self.host)
-        self.send_msg_to('.-.', client, greetings)
+        self.send_msg_to(self.server_id, client, greetings)
 
         while True:
             addressee = client.socket.recv(self.buffer_size).decode('utf8')
@@ -77,9 +95,9 @@ class Server(object):
             msg = client.socket.recv(self.buffer_size).decode('utf8')
 
             if (addressee != 'to_all'):
-                self.send_msg_to(client.name, addressee, msg)
+                self.send_msg_to(client, addressee, msg)
             else:
-                self.send_msg_to_all(client.name, msg)
+                self.send_msg_to_all(client, msg)
 
     def accept_conections(self):
         print('Waiting for connections...')
@@ -87,8 +105,8 @@ class Server(object):
             try:
                 client_socket, client_address = self.server.accept()
                 print('{} has connected'.format(client_address))
-                client = self.Conection('', client_address, client_socket)
-                self.clients[client_address] = client
+                client = self.Conection('', client_address[1], client_socket)
+                self.clients[client_address[1]] = client
                 Thread( target = self.handle_client, args=(client,)).start()
             except KeyboardInterrupt:
                 break
