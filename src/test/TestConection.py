4775c976176d814352ee87cb975dc4bef865a5ce
GrayCentipede <maucarrui@gmail.com>
2018-09-17 20:27:36 -0500

    Updated client unit tests

--- src/test/TestConection.py
+++ src/test/TestConection.py
@@ -8,38 +8,50 @@ from ..Client import Client
 
 class TestConection(unittest.TestCase):
 
+    class Conection(object):
+
+        def __init__(self, cl_socket, cl_address):
+            self.c_socket = cl_socket
+            self.c_address = cl_address
+
     def handle_client(self, client):
         while True:
-            type = client.socket.recv(self.buffer_size).decode('utf8')
-
-            if (type == 'no one'):
-                client.socket.close()
-                break
-
             msg = client.socket.recv(self.buffer_size).decode('utf8')
-            if (type == 'all'):
-                for addressee in self.server_clients:
-                    addressee.socket.send(bytes(prefix + msg).encode('utf8'))
-            else:
-                addressee = self.server_clients[type]
-                addressee.socket.send(bytes(prefix + msg).encode('utf8'))
+
+            instructions = msg.split()
+            event = instructions[0]
+
+            if (event == 'IDENTIFY'):
+                new_name = instructions[1]
+                c = self.clients[client.c_address]
+                self.clients[new_name] = c
+                del self.clients[client.c_address]
+
+            elif (event == 'MESSAGE'):
+                addressee = instructions[1]
+                content = ' '.join(instructions[i] for i in range(2, len(instructions)))
+                addressee_conection = self.clients[addressee]
+                addressee_conection.c_socket.send(bytes(content, 'utf8'))
+
+            elif (event == 'DISCONNECT'):
+                del self.clients[client.c_address]
+                self.server_counter -= 1
+                client.c_socket.close()
 
     def runnable(self):
-        print('Waiting for connections...')
 
         while True:
 
             if (self.server_status == 0):
-                print('The server is now closed')
                 self.server.close()
                 break
 
             try:
                 client_socket, client_address = self.server.accept()
-                client = Client(client_socket, client_name, client_address)
+                client = self.Conection(client_socket, client_address[1])
+                self.clients[client_address] = client
                 self.server_counter += 1
-                print('A client has connected')
-                client_thread = Thread(target=handle_client, args=(client,))
+                client_thread = Thread(target = self.handle_client, args=(client,))
                 client_thread.daemon = True
                 client_thread.start()
 
@@ -47,8 +59,7 @@ class TestConection(unittest.TestCase):
                 print(e)
                 self.assertTrue(False)
 
-
-    def set_up_server(self):
+    def setUp(self):
         self.host = 'localhost'
         self.port = 33000
         self.buffer_size = 1024
@@ -58,53 +69,42 @@ class TestConection(unittest.TestCase):
 
         try:
             self.server.bind(self.address)
-            self.server.listen(0)
+            self.server.listen(10)
             self.server_status = 1
+            self.server_counter = 0
+            self.clients = {}
             self.server_thread = Thread(target = self.runnable)
             self.server_thread.daemon = True
             self.server_thread.start()
+            sleep(0.5)
         except Exception as e:
             print('Failed to set up server.')
             self.assertTrue(server_status)
 
-    def client_connection(self):
-        self.server_clients = {}
-        s = socket()
-        client_1 = Client(s, '...', 'Marco')
-        client_1.connect('localhost', 33000)
-        self.server_clients['Marco'] = client_1
+    def test_client_is_connected(self):
+        client_1 = Client(name = 'Marco', address = 'localhost')
+        client_1.connect(self.host, self.port)
         self.assertTrue(client_1.is_connected())
-        s = socket()
-        client_2 = Client(s, '...', 'Polo')
-        self.server_clients['Polo'] = client_2
-        self.assertTrue(client_2.is_connected())
-        s = socket()
-        client_3 = Client(s, '...', 'Arthur')
-        self.server_clients['Arthur'] = client_3
-        self.assertTrue(client_3.is_connected())
-        client_1.send_msg_to('Hello', 'Polo')
-        msg_list_1 = client_2.get_msg_list_of('Marco')
-        self.assertTrue('Hello' in msg_list_1)
-        client_2.send_msg_to_all('Hello everyone')
-        msg_list_2 = client_1.get_msg_list_of('All')
-        msg_list_3 = client_3.get_msg_list_of('All')
-        self.assertTrue('Hello everyone' in msg_list_2)
-        self.assertTrue('Hello everyone' in msg_list_3)
-        client_1.disconnect()
-        client_2.disconnect()
-        client_3.disconnect()
+        self.assertEqual(self.server_counter, 1)
+
+    def test_client_send_msg(self):
+        self.server_counter = 0
+        client_1 = Client(name = 'Marco', address = 'localhost')
+        client_1.connect(self.host, self.port)
+        client_1.send_msg('IDENTIFY Polo')
+        self.assertEqual(client_1.get_name(), 'Polo')
+        client_2 = Client(name = 'Polo', address = 'localhost')
+        client_1.send_msg('MESSAGE Polo Hello Polo')
+        self.assertTrue('Hello Polo' in client_2.get_last_msgs())
+
+    def test_client_disconnect(self):
+        self.server_counter = 0
+        client_1 = Client(name = 'Marco', address = 'localhost')
+        client_1.connect(self.host, self.port)
+        self.assertEqual(self.server_counter, 1)
+        client_1.send_msg('DISCONNECT')
         self.assertFalse(client_1.is_connected())
-        self.assertFalse(client_2.is_connected())
-        self.assertFalse(client_3.is_connected())
-
-
-    def test_client(self):
-        self.set_up_server()
-        sleep(1)
-        self.client_connection()
-        sleep(1)
-        self.server_status = 0
-        self.server_thread.join()
+        self.assertEqual(self.server_counter, 0)
 
 if __name__ == '__main__':
 
